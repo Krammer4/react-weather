e16e789fd7b56c8c7012d83e05db947c5002e33d
Krammer <bukatovgleb566@gmail.com>
2025-02-01 00:00:57 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Routing } from "./Routing";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Routing />
+    {/* <Routing /> */}
+    <BrowserRouter basename="/react-weather">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
